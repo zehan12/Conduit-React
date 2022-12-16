1d0395f6e86e39110c72b059fe3a5ea6af9a2819
Zehan Khan <73664886+zehan12@users.noreply.github.com>
2022-12-17 01:32:36 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,5 @@
-const base = "https://mighty-oasis-08080.herokuapp.com/api";
+// const base = "https://mighty-oasis-08080.herokuapp.com/api";
+const base = "https://conduit.productionready.io/api"
 
 const header = () => {
     const Token  = localStorage["user_token"] ? `Token ${localStorage["user_token"]}` : "";
